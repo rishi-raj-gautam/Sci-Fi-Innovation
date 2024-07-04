7ff9dc3fe477e3b0b9361d0ea3a3ad3e0d7d3e6d
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2024-07-05 01:50:41 +0530

    event card corrected

--- src/Events.jsx
+++ src/Events.jsx
@@ -1,42 +1,31 @@
-import './event.css'
+import './event.css';
+import React from 'react';
+
 function Events() {
-    return <>
+    const events = [
+        { id: 1, heading: 'Ideathon (upcoming)', location: 'Chandigarh University' },
+        { id: 2, heading: 'Hackathon (upcoming)', location: 'Chandigarh University' },
+        { id: 3, heading: 'NYA', location: 'Chandigarh University' },
+        { id: 4, heading: 'NYA', location: 'Chandigarh University' },
+    ];
+
+    return (
         <div id='events'>
             <div className="container text-center pt-5">
                 <div className='head'><h1>Events</h1></div>
                 <div className="row mt-4">
-                    <div className="col d-flex justify-content-center"><div className="card">
-                        <p className="heading">
-                            Ideathon (upcoming)
-                        </p>
-
-                        <p>Chandigarh University
-                        </p></div></div>
-                    <div className="col d-flex justify-content-center"><div className="card">
-                        <p className="heading">
-                            Hackathon (upcoming)
-                        </p>
-
-                        <p className="para">Chandigarh University
-                        </p></div></div>
-                    <div className="col d-flex justify-content-center"><div className="card">
-                        <p className="heading">
-                            NYA
-                        </p>
-
-                        <p className="para">Chandigarh University
-                        </p></div></div>
-                    <div className="col d-flex justify-content-center"><div className="card">
-                        <p className="heading">
-                            NYA
-                        </p>
-
-                        <p className="para">Chandigarh University
-                        </p></div></div>
+                    {events.map((event) => (
+                        <div key={event.id} className="col d-flex justify-content-center">
+                            <div className="card">
+                                <p className="heading">{event.heading}</p>
+                                <p className="para">{event.location}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-    </>
+    );
 }
 
 export default Events;
