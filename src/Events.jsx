62b757a83b670980c9c9759d4f419ebf207ccea3
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2024-09-11 23:39:27 +0530

    synergy

--- src/Events.jsx
+++ src/Events.jsx
@@ -65,7 +65,7 @@ function Events() {
                             {event.id === 2 && <img src={event.image} alt="Event" className="event-image" />}
                                 <p className="heading">{event.heading}</p>
                                 <p className="para location">{event.location}</p>
-                                {event.id==2?<a href="https://synergy-neon.vercel.app/" className=""><button className="btn-form">Register</button></a>:""}
+                                {event.id==2?<a className=""><button className="btn-form">Closed</button></a>:""}
                                 {event.id==1?<a className=""><button className="btn-form">Closed</button></a>:""}
                                 {event.id==3?<a href="https://forms.gle/WEaTVhTJKxMWznWd8" target='_blank' className=""><button className="btn-form">Register Now</button></a>:""}
                             </div>
